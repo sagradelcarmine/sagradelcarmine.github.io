2fca25ab164496c7a60b83efeb1ed3d2b935e26c
MerloSimone <simone.merlo@studenti.unipd.it>
2023-06-24 00:42:12 +0200

    further modification to make the menu work

--- js/menu.js
+++ js/menu.js
@@ -8,36 +8,58 @@ function Product(photo, name, price, desc,category) {
 }
 
 var prod_list=[];
-prod_list.append(new Product("media/GnocchiAlPomodoro.jpg","Gnocci al Pomodoro",5.00,"Gnocchi al pomodoro","Primi"));
-prod_list.append(new Product("media/GnocchiAlRagu.jpg","Gnocci al Rag\u00F9",5.50,"Gnocchi al rag\u00F9","Primi"));
-prod_list.append(new Product("media/GnocchiAlRaguDiConiglio.jpg","Gnocci al Rag\u00F9 di coniglio",5.50,"Gnocchi al rag\u00F9 di coniglio","Primi"));
-prod_list.append(new Product("media/SapaghettiAlPomodoro.jpg","Spaghetti al Pomodoro",5.00,"spaghetti al pomodoro","Primi"));
-prod_list.append(new Product("media/GnocchiAlRagu.jpg","Gnocci al Rag\u00F9",5.50,"Spaghetti al rag\u00F9","Primi"));
-prod_list.append(new Product("media/GnocchiAlRaguDiConiglio.jpg","Gnocci al Rag\u00F9 di Coniglio",5.50,"Spaghetti al rag\u00F9 di coniglio","Primi"));
+prod_list.push(new Product("media/GnocchiAlPomodoro.jpg","Gnocci al Pomodoro",5.00,"Gnocchi al pomodoro","Primi"));
+prod_list.push(new Product("media/GnocchiAlRagu.jpg","Gnocci al Rag\u00F9",5.50,"Gnocchi al rag\u00F9","Primi"));
+prod_list.push(new Product("media/GnocchiAlRaguDiConiglio.jpg","Gnocci al Rag\u00F9 di coniglio",5.50,"Gnocchi al rag\u00F9 di coniglio","Primi"));
+prod_list.push(new Product("media/SapaghettiAlPomodoro.jpg","Spaghetti al Pomodoro",5.00,"spaghetti al pomodoro","Primi"));
+prod_list.push(new Product("media/GnocchiAlRagu.jpg","Gnocci al Rag\u00F9",5.50,"Spaghetti al rag\u00F9","Primi"));
+prod_list.push(new Product("media/GnocchiAlRaguDiConiglio.jpg","Gnocci al Rag\u00F9 di Coniglio",5.50,"Spaghetti al rag\u00F9 di coniglio","Primi"));
 
 
-prod_list.append(new Product("media/PiattoEstivo.jpg","Piatto Estivo",8.00,"mozzarella di bufala, pomodoro, prosciutto crudo e pane","Secondi"));
-prod_list.append(new Product("media/BisteccaDiCavallo.jpg","Bistecca di Cavallo",10.00,"Bistecca di cavallo con pane","Secondi"));
-prod_list.append(new Product("media/StincoDiMaiale.jpg","Stinco",9.00,"Stinco di maiale al forno con pane","Secondi"));
-prod_list.append(new Product("media/Costicine.jpg","Costicine",7.00,"3 costicine con polenta","Secondi"));
-prod_list.append(new Product("media/Pancetta.jpg","Pancetta alla Piastra",6.00,"Pancetta alla piastra con pane","Secondi"));
-prod_list.append(new Product("media/CosciaDiPollo.jpg","Coscia di Pollo",7.00,"Coscia di pollo disossata con polenta","Secondi"));
-prod_list.append(new Product("media/Salsiccia.jpg","Salsiccia",6.00,"2 salsicce con polenta","Secondi"));
-prod_list.append(new Product("media/Tagliata.png","Tagliata",11.00,"Tagliata di manzo con rucola e pane","Secondi"));
-prod_list.append(new Product("media/PiattoSagra.jpg","Piatto Sagra",10.00,"2 costicine, salsiccia, pancetta e polenta","Secondi"));
-prod_list.append(new Product("media/PiattoBimbi.jpg","Piatto Bimbi",5.00,"Wurstel con patate","Secondi"));
-prod_list.append(new Product("media/FormaggioCotto.jpg","Formaggio Cotto",6.00,"Formaggio cotto con polenta","Secondi"));
-prod_list.append(new Product("media/FritturaDiAnelli.jpg","Frittura di Anelli",10.00,"Frittura di anelli di totano con polenta","Secondi"));
+prod_list.push(new Product("media/PiattoEstivo.jpg","Piatto Estivo",8.00,"mozzarella di bufala, pomodoro, prosciutto crudo e pane","Secondi"));
+prod_list.push(new Product("media/BisteccaDiCavallo.jpg","Bistecca di Cavallo",10.00,"Bistecca di cavallo con pane","Secondi"));
+prod_list.push(new Product("media/StincoDiMaiale.jpg","Stinco",9.00,"Stinco di maiale al forno con pane","Secondi"));
+prod_list.push(new Product("media/Costicine.jpg","Costicine",7.00,"3 costicine con polenta","Secondi"));
+prod_list.push(new Product("media/Pancetta.jpg","Pancetta alla Piastra",6.00,"Pancetta alla piastra con pane","Secondi"));
+prod_list.push(new Product("media/CosciaDiPollo.jpg","Coscia di Pollo",7.00,"Coscia di pollo disossata con polenta","Secondi"));
+prod_list.push(new Product("media/Salsiccia.jpg","Salsiccia",6.00,"2 salsicce con polenta","Secondi"));
+prod_list.push(new Product("media/Tagliata.png","Tagliata",11.00,"Tagliata di manzo con rucola e pane","Secondi"));
+prod_list.push(new Product("media/PiattoSagra.jpg","Piatto Sagra",10.00,"2 costicine, salsiccia, pancetta e polenta","Secondi"));
+prod_list.push(new Product("media/PiattoBimbi.jpg","Piatto Bimbi",5.00,"Wurstel con patate","Secondi"));
+prod_list.push(new Product("media/FormaggioCotto.jpg","Formaggio Cotto",6.00,"Formaggio cotto con polenta","Secondi"));
+prod_list.push(new Product("media/FritturaDiAnelli.jpg","Frittura di Anelli",10.00,"Frittura di anelli di totano con polenta","Secondi"));
 
 
+prod_list.push(new Product("media/Patate.jpg","Patatine Fritte",2.50,"Patatine fritte","Contorni"));
+prod_list.push(new Product("media/Funghi.jpg","Funghi",3.00,"Funghi","Contorni"));
+prod_list.push(new Product("media/Fagioli.jpg","Fagioli",3.00,"Fagioli","Contorni"));
+prod_list.push(new Product("media/Patate.jpg","Patatine Fritte (BAR)",2.50,"Patatine fritte da ritirare presso il bar","Contorni"));
+
+prod_list.push(new Product("media/PaninoOnto.jpg","Panino ONTO",6.00,"Panino con pastin, cipolla e peperoni","Panini"));
+prod_list.push(new Product("media/PaninoPastin.jpg","Panino PASTIN",5.50,"Panino con pastin","Panini"));
+prod_list.push(new Product("media/PaninoVegetariano.jpg","Panino VEGETARIANO",6.00,"Panino con cipolla e peperoni","Panini"));
+prod_list.push(new Product("media/PaninoPorchetta.jpg","Panino PORCHETTA",4.00,"Panino con porchetta","Panini"));
+
+prod_list.push(new Product("media/Acqua.jpg","Acqua minerale 0.5lt",1.00,"Acqua minerale naturale o frizzante","Bevande"));
+prod_list.push(new Product("media/Bibite.jpg","Bibite",2.00,"coca cola, aranciata, lemonsoda o the","Bevande"));
+prod_list.push(new Product("media/Succo.jpg","Succo di Frutta",2.00,"Succo di frutta, gusti assortiti","Bevande"));
+prod_list.push(new Product("media/Caffe.jpg","Caff\u00E8",1.00,"Caff\u00E8","Bevande"));
+prod_list.push(new Product("media/Ombra.jpg","Ombra di Vino",1.00,"Ombra di vino rosso o bianco","Bevande"));
+prod_list.push(new Product("media/Vino.jpg","Vino in Bottiglia SFUSO (0.75lt)",5.00,"Vino in Bottiglia SFUSO (0.75lt)","Bevande"));
+prod_list.push(new Product("media/Vino.jpg","Vino in Bottiglia (0.75lt)",8.00,"Vino in Bottiglia (0.75lt)","Bevande"));
+prod_list.push(new Product("media/Prosecco.jpg","Prosecco DOC Treviso BRUT",10.00,"Prosecco DOC Treviso BRUT","Bevande"));
+
+prod_list.push(new Product("media/BirraCastelloBionda.jpg","Birra BIONDA 0,25cc",2.50,"Birra bionda Castello, bicchiere da 0,25cc","Bevande"));
+prod_list.push(new Product("media/BirraPedavenaNF.jpg","Birra NON FILTRATA 0,25cc",2.50,"Birra non filtrata Dolomite, bicchiere da 0,25cc","Bevande"));
+prod_list.push(new Product("media/BirraCastelloRossa.jpg","Birra ROSSA 0,25cc",2.50,"Birra rossa Castello, bicchiere da 0,25cc","Bevande"));
+prod_list.push(new Product("media/BirraCastelloBionda.jpg","Birra BIONDA 0,4cc",4.50,"Birra bionda Castello, bicchiere da 0,4cc","Bevande"));
+prod_list.push(new Product("media/BirraPedavenaNF.jpg","Birra NON FILTRATA 0,4cc",4.50,"Birra non filtrata Dolomite, bicchiere da 0,4cc","Bevande"));
+prod_list.push(new Product("media/BirraCastelloRossa.jpg","Birra ROSSA 0,4cc",4.50,"Birra rossa Castello, bicchiere da 0,4cc","Bevande"));
+prod_list.push(new Product("media/Caraffa.jpg","Caraffa di BIRRA 1,5lt",12.00,"Caraffa di birra (1,5lt) Rossa,Bionda o non filtrata","Bevande"));
 
 
-
-
-
-
-
-
+prod_list.push(new Product("media/Dolci.jpg","Fetta di dolce",2.50,"Profiteroles, torta della nonna o torta al cioccolato","Dolci"));
+prod_list.push(new Product("media/Sgroppino.jpg","Sgroppino",2.50,"Sgroppino gusto limone o liquirizia","Dolci"));
 
 
 //declaring shopping cart
@@ -64,6 +86,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 function generate_categories_list(categoriesList){
     var categoriesdiv=document.getElementById("categories-list");
+    var maincon=document.getElementById("full-content");
     if(categoriesList.length > 0){
         let firstCategory; //needed to load the first elements.
         for(var i=0; i<categoriesList.length; i++){
@@ -79,6 +102,16 @@ function generate_categories_list(categoriesList){
             categorya.href="#"+cat;
             categoriesdiv.appendChild(categorya);
 
+
+            var categorytitle=document.createElement("h3");
+            categorytitle.innerText=cat;
+            categorytitle.classList="category-title";
+            categorytitle.id=cat;
+            var categorydiv=document.createElement("div");
+            categorydiv.id=cat+"-div";
+            maincon.appendChild(categorytitle);
+            maincon.appendChild(categorydiv);
+            generate_product_list_by_categories(prod_list,cat);
         }
 
 
@@ -91,89 +124,61 @@ function generate_categories_list(categoriesList){
 
 }
 
-function generateProductHandler(url){
-    return function() {
-        //clear_products_list();
-        generate_products_list(url);
 
-    };
-}
 
-function clear_products_list(){
-    var productListElement = document.getElementById("products_list");
-    while (productListElement.firstChild) {
-        productListElement.removeChild(productListElement.firstChild);
-    }
-}
-function generate_products_list(url_products){
-    let url = url_products;
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.onload=function (){
-        if(xhr.status===200){
-            var responseObject = JSON.parse(xhr.responseText);
-            const productObject = responseObject['resource-list'].map(item =>item.product);
-            product_list = productObject.map(product=>[product.name,product.price,product.photo,product.description]);
-            generate_product_list_by_categories(product_list);
 
-        }else{
-            printError("Unable to load products!",20);
-        }
-    }
-    xhr.send();
+function generate_product_list_by_categories(productsList,category){
 
-}
-
-function generate_product_list_by_categories(productsList){
-
-    var productContainer = document.getElementById("products-container");
+    var productContainer = document.getElementById(category+"-div");
     productContainer.innerHTML = "";
     for (let i = 0; i < productsList.length; i++) {
         (function () {
             var product = productsList[i];
-            var name = product[0];
-            var price = product[1].toFixed(2);
-            var photo = product[2];
+            if(product.category===category){
+                var name = product.name;
+                var price = product.price.toFixed(2);
+                var photo = product.photo;
 
-            var row = document.createElement("div");
-            row.classList.add("product-row");
-            row.classList.add("row");
-            row.classList.add("gx-0");
-            row.classList.add("shadow-sm", "bg-white", "rounded", "align-items-center");
-            row.setAttribute("data-bs-toggle", "modal");
-            row.setAttribute("data-bs-target", "#staticBackdrop");
-            row.addEventListener("click", function () {
-                modifyPopup(product);
-            });
-
-
-            //Adding the image
-            let col_img = document.createElement("div");
-            col_img.classList.add("col");
-            let imageElement = document.createElement("img");
-            console.log(baseURL);
-            imageElement.src = photo || baseURL + "/media/dish_default.png";
-            imageElement.alt = "";
-            imageElement.classList.add("product-image");
-            col_img.appendChild(imageElement);
-            row.appendChild(col_img);
-
-            //Adding the name
-            let col_name = document.createElement("div");
-            col_name.classList.add("col", "text-center", "prod-name");
-            col_name.innerHTML = name;
-
-            row.appendChild(col_name);
-            //Adding the price
-            let col_price = document.createElement("div");
-            col_price.classList.add("col","price-col", "text-center");
-            col_price.innerHTML = Number(price).toFixed(2)+" \u20AC";
-
-            row.appendChild(col_price);
+                var row = document.createElement("div");
+                row.classList.add("product-row");
+                row.classList.add("row");
+                row.classList.add("gx-0");
+                row.classList.add("shadow-sm", "bg-white", "rounded", "align-items-center");
+                row.setAttribute("data-bs-toggle", "modal");
+                row.setAttribute("data-bs-target", "#staticBackdrop");
+                row.addEventListener("click", function () {
+                    modifyPopup(product);
+                });
 
 
-            productContainer.appendChild(row);
+                //Adding the image
+                let col_img = document.createElement("div");
+                col_img.classList.add("col");
+                let imageElement = document.createElement("img");
+                console.log(baseURL);
+                imageElement.src = photo;
+                imageElement.alt = "";
+                imageElement.classList.add("product-image");
+                col_img.appendChild(imageElement);
+                row.appendChild(col_img);
+
+                //Adding the name
+                let col_name = document.createElement("div");
+                col_name.classList.add("col", "text-center", "prod-name");
+                col_name.innerHTML = name;
+
+                row.appendChild(col_name);
+                //Adding the price
+                let col_price = document.createElement("div");
+                col_price.classList.add("col","price-col", "text-center");
+                col_price.innerHTML = Number(price).toFixed(2)+" \u20AC";
+
+                row.appendChild(col_price);
+
+
+                productContainer.appendChild(row);
+            }
+
         })();
 
 
@@ -183,60 +188,20 @@ function generate_product_list_by_categories(productsList){
 }
 function modifyPopup(product) {
     var popupTitle = $("#staticBackdropLabel");
-    popupTitle.html(product[0]);
+    popupTitle.html(product.name);
     var dishDescription = $("#dishDescription");
     dishDescription.html("");
-    dishDescription.html(product[3]);
-    var minusQuantity = $("#minusbut");
-    var plusQuantity = $("#plusbut");
-    var closebutton = $("#closebut");
-    var modalCloseButton = $('[data-bs-dismiss="modal"]');
-    var addToCartButton = $("#addbut");
+    dishDescription.html(product.desc);
 
-    function decreaseQuantityWrapper() {
-        decreaseQuantity(addToCartButton);
-    }
+    var dishImage = $("#dish-image");
+    dishImage.attr("src",product.photo);
+    dishImage.attr("alt",product.name);
+    dishImage.attr("title",product.name);
 
-    function increaseQuantityWrapper() {
-        increaseQuantity(addToCartButton);
-    }
 
-    minusQuantity.off();
-    plusQuantity.off();
 
-    addToCartButton.attr("data-name", product[0]);
-    addToCartButton.attr("data-price", product[1]);
-    addToCartButton.attr("data-quantity", 1);
-
-    minusQuantity.on("click", decreaseQuantityWrapper);
-    plusQuantity.on("click", increaseQuantityWrapper);
-    closebutton.on("click", resetQuantity);
-    modalCloseButton.on("click", resetQuantity);
-}
-function resetQuantity() {
-    document.getElementById("quantity").textContent = 1;
 }
 
-
-function increaseQuantity(button) {
-    var quantityElement = $("#quantity");
-    var currentQuantity = quantityElement.text();
-    var quantityValue = parseInt(currentQuantity);
-    //console.log("increasing");
-    quantityElement.text(quantityValue + 1);
-    button.attr("data-quantity", quantityValue + 1);
-}
-
-function decreaseQuantity(button) {
-    var quantityElement = $("#quantity");
-    var currentQuantity = quantityElement.text();
-    var quantityValue = parseInt(currentQuantity);
-    if (quantityValue > 1) {
-        //console.log("decreasing");
-        quantityElement.text(quantityValue - 1);
-        button.attr("data-quantity", quantityValue - 1);
-    }
-}
 document.addEventListener("DOMContentLoaded", function() {
     shoppingCart = (function () {
         // =============================
@@ -776,3 +741,22 @@ function printError(error,time){
         $('#error-alert').text("").css("display","none");
     },time*1000);
 }
+
+
+var btn_scroll_top;
+window.onload = function(){
+    btn_scroll_top = document.getElementById("btn_scroll-top");
+    btn_scroll_top.addEventListener("click", function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
+}
+window.onscroll = function () {
+    if (window.pageYOffset > 20)
+        btn_scroll_top.style.display = "block";
+    else
+        btn_scroll_top.style.display = "none";
+};
+
