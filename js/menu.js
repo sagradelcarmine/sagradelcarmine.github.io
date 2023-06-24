d0816738da1a9ab2a23347a3af8cb6322182a21c
MerloSimone <simone.merlo@studenti.unipd.it>
2023-06-24 11:49:31 +0200

    Full menu with images

--- js/menu.js
+++ js/menu.js
@@ -8,58 +8,58 @@ function Product(photo, name, price, desc,category) {
 }
 
 var prod_list=[];
-prod_list.push(new Product("media/menu/GnocchiAlPomodoro.jpg","Gnocci al Pomodoro",5.00,"Gnocchi al pomodoro","Primi"));
-prod_list.push(new Product("media/menu/GnocchiAlRagu.jpg","Gnocci al Rag\u00F9",5.50,"Gnocchi al rag\u00F9","Primi"));
-prod_list.push(new Product("media/menu/GnocchiAlRaguDiConiglio.jpg","Gnocci al Rag\u00F9 di coniglio",5.50,"Gnocchi al rag\u00F9 di coniglio","Primi"));
-prod_list.push(new Product("media/menu/SapaghettiAlPomodoro.jpg","Spaghetti al Pomodoro",5.00,"spaghetti al pomodoro","Primi"));
-prod_list.push(new Product("media/menu/GnocchiAlRagu.jpg","Gnocci al Rag\u00F9",5.50,"Spaghetti al rag\u00F9","Primi"));
-prod_list.push(new Product("media/menu/GnocchiAlRaguDiConiglio.jpg","Gnocci al Rag\u00F9 di Coniglio",5.50,"Spaghetti al rag\u00F9 di coniglio","Primi"));
+prod_list.push(new Product("media/menu/GnocchiAlPomodoro.png","Gnocci al Pomodoro",5.00,"Gnocchi al pomodoro","Primi"));
+prod_list.push(new Product("media/menu/GnocchiAlRagu.png","Gnocci al Rag\u00F9",5.50,"Gnocchi al rag\u00F9","Primi"));
+prod_list.push(new Product("media/menu/GnocchiAlRaguDiConiglio.png","Gnocci al Rag\u00F9 di coniglio",5.50,"Gnocchi al rag\u00F9 di coniglio","Primi"));
+prod_list.push(new Product("media/menu/SpaghettiAlPomodoro.png","Spaghetti al Pomodoro",5.00,"spaghetti al pomodoro","Primi"));
+prod_list.push(new Product("media/menu/SpaghettiAlRagu.png","Spaghetti al Rag\u00F9",5.50,"Spaghetti al rag\u00F9","Primi"));
+prod_list.push(new Product("media/menu/SpaghettiAlRaguDiConiglio.png","Spaghetti al Rag\u00F9 di Coniglio",5.50,"Spaghetti al rag\u00F9 di coniglio","Primi"));
 
 
-prod_list.push(new Product("media/menu/PiattoEstivo.jpg","Piatto Estivo",8.00,"mozzarella di bufala, pomodoro, prosciutto crudo e pane","Secondi"));
-prod_list.push(new Product("media/menu/BisteccaDiCavallo.jpg","Bistecca di Cavallo",10.00,"Bistecca di cavallo con pane","Secondi"));
-prod_list.push(new Product("media/menu/StincoDiMaiale.jpg","Stinco",9.00,"Stinco di maiale al forno con pane","Secondi"));
-prod_list.push(new Product("media/menu/Costicine.jpg","Costicine",7.00,"3 costicine con polenta","Secondi"));
-prod_list.push(new Product("media/menu/Pancetta.jpg","Pancetta alla Piastra",6.00,"Pancetta alla piastra con pane","Secondi"));
-prod_list.push(new Product("media/menu/CosciaDiPollo.jpg","Coscia di Pollo",7.00,"Coscia di pollo disossata con polenta","Secondi"));
-prod_list.push(new Product("media/menu/Salsiccia.jpg","Salsiccia",6.00,"2 salsicce con polenta","Secondi"));
+prod_list.push(new Product("media/menu/PiattoEstivo.png","Piatto Estivo",8.00,"Mozzarella di bufala, pomodoro, prosciutto crudo e pane","Secondi"));
+prod_list.push(new Product("media/menu/BisteccaDiCavallo.png","Bistecca di Cavallo",10.00,"Bistecca di cavallo con pane","Secondi"));
+prod_list.push(new Product("media/menu/Stinco.png","Stinco",9.00,"Stinco di maiale al forno con pane","Secondi"));
+prod_list.push(new Product("media/menu/Costicine.png","Costicine",7.00,"3 costicine con polenta","Secondi"));
+prod_list.push(new Product("media/menu/Pancetta.png","Pancetta alla Piastra",6.00,"Pancetta alla piastra con pane","Secondi"));
+prod_list.push(new Product("media/menu/CosciaDiPollo.png","Coscia di Pollo",7.00,"Coscia di pollo disossata con polenta","Secondi"));
+prod_list.push(new Product("media/menu/Salsiccia.png","Salsiccia",6.00,"2 salsicce con polenta","Secondi"));
 prod_list.push(new Product("media/menu/Tagliata.png","Tagliata",11.00,"Tagliata di manzo con rucola e pane","Secondi"));
-prod_list.push(new Product("media/menu/PiattoSagra.jpg","Piatto Sagra",10.00,"2 costicine, salsiccia, pancetta e polenta","Secondi"));
-prod_list.push(new Product("media/menu/PiattoBimbi.jpg","Piatto Bimbi",5.00,"Wurstel con patate","Secondi"));
-prod_list.push(new Product("media/menu/FormaggioCotto.jpg","Formaggio Cotto",6.00,"Formaggio cotto con polenta","Secondi"));
-prod_list.push(new Product("media/menu/FritturaDiAnelli.jpg","Frittura di Anelli",10.00,"Frittura di anelli di totano con polenta","Secondi"));
+prod_list.push(new Product("media/menu/PiattoSagra.png","Piatto Sagra",10.00,"2 costicine, salsiccia, pancetta e polenta","Secondi"));
+prod_list.push(new Product("media/menu/PiattoBimbi.png","Piatto Bimbi",5.00,"Wurstel con patate","Secondi"));
+prod_list.push(new Product("media/menu/FormaggioCotto.png","Formaggio Cotto",6.00,"Formaggio cotto con polenta","Secondi"));
+prod_list.push(new Product("media/menu/FritturaDiAnelli.png","Frittura di Anelli",10.00,"Frittura di anelli di totano con polenta","Secondi"));
 
 
-prod_list.push(new Product("media/menu/Patate.jpg","Patatine Fritte",2.50,"Patatine fritte","Contorni"));
-prod_list.push(new Product("media/menu/Funghi.jpg","Funghi",3.00,"Funghi","Contorni"));
-prod_list.push(new Product("media/menu/Fagioli.jpg","Fagioli",3.00,"Fagioli","Contorni"));
-prod_list.push(new Product("media/menu/Patate.jpg","Patatine Fritte (BAR)",2.50,"Patatine fritte da ritirare presso il bar","Contorni"));
+prod_list.push(new Product("media/menu/Patate.png","Patatine Fritte",2.50,"Patatine fritte","Contorni"));
+prod_list.push(new Product("media/menu/Funghi.png","Funghi",3.00,"Funghi","Contorni"));
+prod_list.push(new Product("media/menu/Fagioli.png","Fagioli",3.00,"Fagioli","Contorni"));
+prod_list.push(new Product("media/menu/Patate.png","Patatine Fritte (BAR)",2.50,"Patatine fritte da ritirare presso il bar","Contorni"));
 
-prod_list.push(new Product("media/menu/PaninoOnto.jpg","Panino ONTO",6.00,"Panino con pastin, cipolla e peperoni","Panini"));
-prod_list.push(new Product("media/menu/PaninoPastin.jpg","Panino PASTIN",5.50,"Panino con pastin","Panini"));
-prod_list.push(new Product("media/menu/PaninoVegetariano.jpg","Panino VEGETARIANO",6.00,"Panino con cipolla e peperoni","Panini"));
-prod_list.push(new Product("media/menu/PaninoPorchetta.jpg","Panino PORCHETTA",4.00,"Panino con porchetta","Panini"));
+prod_list.push(new Product("media/menu/PaninoOnto.png","Panino ONTO",6.00,"Panino con pastin, cipolla e peperoni","Panini"));
+prod_list.push(new Product("media/menu/PaninoPastin.png","Panino PASTIN",5.50,"Panino con pastin","Panini"));
+prod_list.push(new Product("media/menu/PaninoVegetariano.png","Panino VEGETARIANO",6.00,"Panino con cipolla e peperoni","Panini"));
+prod_list.push(new Product("media/menu/PaninoPorchetta.png","Panino PORCHETTA",4.00,"Panino con porchetta","Panini"));
 
-prod_list.push(new Product("media/menu/Acqua.jpg","Acqua minerale 0.5lt",1.00,"Acqua minerale naturale o frizzante","Bevande"));
-prod_list.push(new Product("media/menu/Bibite.jpg","Bibite",2.00,"coca cola, aranciata, lemonsoda o the","Bevande"));
-prod_list.push(new Product("media/menu/Succo.jpg","Succo di Frutta",2.00,"Succo di frutta, gusti assortiti","Bevande"));
-prod_list.push(new Product("media/menu/Caffe.jpg","Caff\u00E8",1.00,"Caff\u00E8","Bevande"));
-prod_list.push(new Product("media/menu/Ombra.jpg","Ombra di Vino",1.00,"Ombra di vino rosso o bianco","Bevande"));
-prod_list.push(new Product("media/menu/Vino.jpg","Vino in Bottiglia SFUSO (0.75lt)",5.00,"Vino in Bottiglia SFUSO (0.75lt)","Bevande"));
-prod_list.push(new Product("media/menu/Vino.jpg","Vino in Bottiglia (0.75lt)",8.00,"Vino in Bottiglia (0.75lt)","Bevande"));
-prod_list.push(new Product("media/menu/Prosecco.jpg","Prosecco DOC Treviso BRUT",10.00,"Prosecco DOC Treviso BRUT","Bevande"));
+prod_list.push(new Product("media/menu/Acqua.png","Acqua minerale 0.5lt",1.00,"Acqua minerale naturale o frizzante","Bevande"));
+prod_list.push(new Product("media/menu/Bibite.png","Bibite",2.00,"coca cola, aranciata, lemonsoda o the","Bevande"));
+prod_list.push(new Product("media/menu/Succo.png","Succo di Frutta",2.00,"Succo di frutta, gusti assortiti","Bevande"));
+prod_list.push(new Product("media/menu/Caffe.png","Caff\u00E8",1.00,"Caff\u00E8","Bevande"));
+prod_list.push(new Product("media/menu/Ombra.png","Ombra di Vino",1.00,"Ombra di vino rosso o bianco","Bevande"));
+prod_list.push(new Product("media/menu/Vino.png","Vino in Bottiglia SFUSO (0.75lt)",5.00,"Vino in Bottiglia SFUSO (0.75lt)","Bevande"));
+prod_list.push(new Product("media/menu/VinoBott.png","Vino in Bottiglia (0.75lt)",8.00,"Vino in Bottiglia (0.75lt)","Bevande"));
+prod_list.push(new Product("media/menu/Prosecco.png","Prosecco DOC Treviso BRUT",10.00,"Prosecco DOC Treviso BRUT","Bevande"));
 
-prod_list.push(new Product("media/menu/BirraCastelloBionda.jpg","Birra BIONDA 0,25cc",2.50,"Birra bionda Castello, bicchiere da 0,25cc","Bevande"));
-prod_list.push(new Product("media/menu/BirraPedavenaNF.jpg","Birra NON FILTRATA 0,25cc",2.50,"Birra non filtrata Dolomite, bicchiere da 0,25cc","Bevande"));
-prod_list.push(new Product("media/menu/BirraCastelloRossa.jpg","Birra ROSSA 0,25cc",2.50,"Birra rossa Castello, bicchiere da 0,25cc","Bevande"));
-prod_list.push(new Product("media/menu/BirraCastelloBionda.jpg","Birra BIONDA 0,4cc",4.50,"Birra bionda Castello, bicchiere da 0,4cc","Bevande"));
-prod_list.push(new Product("media/menu/BirraPedavenaNF.jpg","Birra NON FILTRATA 0,4cc",4.50,"Birra non filtrata Dolomite, bicchiere da 0,4cc","Bevande"));
-prod_list.push(new Product("media/menu/BirraCastelloRossa.jpg","Birra ROSSA 0,4cc",4.50,"Birra rossa Castello, bicchiere da 0,4cc","Bevande"));
-prod_list.push(new Product("media/menu/Caraffa.jpg","Caraffa di BIRRA 1,5lt",12.00,"Caraffa di birra (1,5lt) Rossa,Bionda o non filtrata","Bevande"));
+prod_list.push(new Product("media/menu/BirraCastelloBionda.png","Birra BIONDA 0,25cc",2.50,"Birra bionda Castello, bicchiere da 0,25cc","Bevande"));
+prod_list.push(new Product("media/menu/BirraPedavenaNF.png","Birra NON FILTRATA 0,25cc",2.50,"Birra non filtrata Dolomite, bicchiere da 0,25cc","Bevande"));
+prod_list.push(new Product("media/menu/BirraCastelloRossa.png","Birra ROSSA 0,25cc",2.50,"Birra rossa Castello, bicchiere da 0,25cc","Bevande"));
+prod_list.push(new Product("media/menu/BirraCastelloBionda.png","Birra BIONDA 0,4cc",4.50,"Birra bionda Castello, bicchiere da 0,4cc","Bevande"));
+prod_list.push(new Product("media/menu/BirraPedavenaNF.png","Birra NON FILTRATA 0,4cc",4.50,"Birra non filtrata Dolomite, bicchiere da 0,4cc","Bevande"));
+prod_list.push(new Product("media/menu/BirraCastelloRossa.png","Birra ROSSA 0,4cc",4.50,"Birra rossa Castello, bicchiere da 0,4cc","Bevande"));
+prod_list.push(new Product("media/menu/Caraffa.png","Caraffa di BIRRA 1,5lt",12.00,"Caraffa di birra (1,5lt) Rossa, Bionda o non filtrata","Bevande"));
 
 
-prod_list.push(new Product("media/menu/Dolci.jpg","Fetta di dolce",2.50,"Profiteroles, torta della nonna o torta al cioccolato","Dolci"));
-prod_list.push(new Product("media/menu/Sgroppino.jpg","Sgroppino",2.50,"Sgroppino gusto limone o liquirizia","Dolci"));
+prod_list.push(new Product("media/menu/Dolci.png","Fetta di dolce",2.50,"Profiteroles, torta della nonna o torta al cioccolato","Dolci"));
+prod_list.push(new Product("media/menu/Sgroppino.png","Sgroppino",2.50,"Sgroppino gusto limone o liquirizia","Dolci"));
 
 
 //declaring shopping cart
